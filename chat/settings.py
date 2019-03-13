a545866557115c18c32b9c5812d0b1c626d2466e
ju1900 <1044161599@qq.com>
2019-03-14 00:06:59 +0800

    initiate repo

--- chat/settings.py
+++ chat/settings.py
@@ -55,7 +55,7 @@ TEMPLATES = (
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default="postgres:///channels-example", conn_max_age=500)
+    'default': dj_database_url.config(default="postgres:///myproject", conn_max_age=500)
 }
 
 AUTH_PASSWORD_VALIDATORS = (
